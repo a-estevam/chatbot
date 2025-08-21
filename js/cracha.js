6ac795f3790d6cca17fae66577450bc48b84e110
a-estevam <estevam.adesign@gmail.com>
2025-08-20 22:19:20 -0300

    refactor: renomeia arquivos para seguir padrão de nomenclatura

--- js/cracha.js
+++ js/cracha.js
@@ -1,11 +1,12 @@
 const badge = document.querySelector(".final_cut")
 const bleed = document.querySelector(".bleed_area")
+const measure = document.querySelector(".measure")
 
 const bgcolor = "green"
 const radius = 8
 
 
-
+//crachá tamanho final
 const badgeWidth = 54
 const badgeheight = 85.6
 
@@ -15,7 +16,7 @@ badge.style.border = "2px solid black";
 badge.style.borderRadius = radius + "px"
 
 
-
+//sangria do cracha
 const bleedWidth = 60
 const bleedheight = 90.6
 
@@ -23,3 +24,4 @@ bleed.style.width = bleedWidth + "mm"
 bleed.style.height = bleedheight + "mm"
 bleed.style.backgroundColor = bgcolor
 bleed.style.border = "2px solid green";
+
