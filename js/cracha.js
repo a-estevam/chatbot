b24e214ceefb18950f8691dec0a0347cca40d9d4
a-estevam <estevam.adesign@gmail.com>
2025-08-20 21:49:33 -0300

    refactor: renomeia arquivos para seguir padrão de nomenclatura

---
+++ js/cracha.js
@@ -0,0 +1,25 @@
+const badge = document.querySelector(".final_cut")
+const bleed = document.querySelector(".bleed_area")
+
+const bgcolor = "green"
+const radius = 8
+
+
+
+const badgeWidth = 54
+const badgeheight = 85.6
+
+badge.style.width = badgeWidth + "mm"
+badge.style.height = badgeheight + "mm"
+badge.style.border = "2px solid black";
+badge.style.borderRadius = radius + "px"
+
+
+
+const bleedWidth = 60
+const bleedheight = 90.6
+
+bleed.style.width = bleedWidth + "mm"
+bleed.style.height = bleedheight + "mm"
+bleed.style.backgroundColor = bgcolor
+bleed.style.border = "2px solid green";
